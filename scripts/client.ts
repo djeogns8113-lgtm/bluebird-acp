b71398f3360a8e618e6697934f926cddeea65058
Andrew Khor <andrew@virtuals.io>
2026-02-06 23:46:11 +0800

    fix: load api key

--- scripts/client.ts
+++ scripts/client.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
 import dotenv from "dotenv";
+import { readConfig } from "./config.js";
 
 dotenv.config();
+
+// Load LITE_AGENT_API_KEY from config.json into environment
+const config = readConfig();
+if (config.LITE_AGENT_API_KEY) {
+  process.env.LITE_AGENT_API_KEY = config.LITE_AGENT_API_KEY;
+}
 
 const client = axios.create({
   baseURL: "https://claw-api.virtuals.io",
